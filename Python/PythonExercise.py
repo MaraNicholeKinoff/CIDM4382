179ab1ca6f12f9c36656bacf8a6873c5c3a0326d
MaraNicholeKinoff <maranicholemayfield@gmail.com>
2018-10-15 13:59:37 -0500

    Update 10/15/2018

--- Python/PythonExercise.py
+++ Python/PythonExercise.py
@@ -1,6 +1,18 @@
 import math
 import random
 
+def f_to_c(f):
+    c = (f - 32) * (5/9)
+    return c
+print(f_to_c(32))
+print(f_to_c(85))
+
+def add (x, y):
+    z = x + y
+    return z
+
+print(add(add(1,1),add(2,2)))
+
 # Round 1
 print("Hello World!")
 
